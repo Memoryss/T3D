ccc62e5d33c007d820a9f60bae2f2beee95e7ff1
shentongle <1275507634@qq.com>
2017-02-24 00:41:20 +0800

    window

--- src/T3DWindow.cpp
+++ src/T3DWindow.cpp
@@ -40,10 +40,72 @@ namespace T3D {
 		return DefWindowProc(hwnd, msg, wparam, lparam);
 	}
 
-	Window::Window(int width, int height, LPSTR title)	
+	Window::Window(int width, int height, LPSTR title) : m_width(width), m_height(height), m_title(title)
 	{
-		WNDCLASS ws = { CS_DBLCLKS | CS_OWNDC | CS_HREDRAW | CS_VREDRAW , WindowProc, 0, 0, 0, NULL, NULL, NULL, NULL, _T(title) };
 		
+	}
 
+	bool Window::Init()
+	{
+		WNDCLASS wc = { CS_DBLCLKS | CS_OWNDC | CS_HREDRAW | CS_VREDRAW , WindowProc, 0, 0, 0, NULL, NULL, NULL, NULL, _T(m_title.c_str()) };
+
+		// -m_height表示top - bottom
+		BITMAPINFO bi = { {sizeof(BITMAPINFOHEADER), m_width, -m_height, 1, 32, BI_RGB, m_width * m_height * 4, 0, 0, 0, 0} };
+
+		RECT rect = { 0, 0, m_width, m_height };
+
+		// 关闭窗口相关
+		Close();
+
+		wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+		wc.hInstance = GetModuleHandle(NULL);  //NULL表示本程序的instance
+		wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+		if (!RegisterClass(&wc)) 
+		{
+			return false;
+		}
+
+		//创建窗口
+		m_handle = CreateWindow(_T("WINDOW"), m_title.c_str(), WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION, 0, 0, 0, 0, NULL, NULL, wc.hInstance, NULL);
+		if (m_handle == 0) return false;
+
+		HDC hDC = GetDC(m_handle);
+		m_hdc = CreateCompatibleDC(hDC);
+		ReleaseDC(m_handle, hDC);
+
+		//创建应用程序可以直接写入的，与设备无关的位图
+		m_hb = CreateDIBSection(m_hdc, &bi, DIB_RGB_COLORS, m_framebuffer, 0, 0);
+		if (m_ob == NULL) return false;
+
+		m_ob = (HBITMAP)SelectObject(m_hdc, m_hb);
+
+		return true;
+	}
+
+	void Window::Close()
+	{
+		if (m_hdc)
+		{
+			if (m_ob)
+			{
+				//选择一个对象到制定的设备上下文中，新对象替换先前相同类型的对象
+				SelectObject(m_hdc, m_ob);
+				m_ob = NULL;
+			}
+			DeleteDC(m_hdc);
+			m_hdc = NULL;
+		}
+
+		if (m_hb)
+		{
+			DeleteObject(m_hb);
+			m_hb = NULL;
+		}
+
+		if (m_handle)
+		{
+			CloseWindow(m_handle);
+			m_handle = NULL;
+		}
 	}
 }
--- src/T3DWindow.h
+++ src/T3DWindow.h
@@ -2,6 +2,7 @@
 #define __T3D_WINDOW_H__
 
 #include <Windows.h>
+#include <string>
 
 namespace T3D {
 
@@ -10,15 +11,21 @@ namespace T3D {
 	public:
 		Window(int width = 800, int height = 600, LPSTR title = "");
 
+		bool Init();
+
+		//关闭HDC和窗口等
+		void Close();
+
 	private:
 		HWND m_handle; //窗口句柄
 		HDC m_hdc; //设备上下文
 		HBITMAP m_hb;
 		HBITMAP m_ob;
-		LPVOID m_framebuffer;
+		LPVOID *m_framebuffer;
 
 		int m_width; //窗口宽度 
 		int m_height; //窗口高度
+		std::string m_title; //窗口名称
 	};
 
 }
