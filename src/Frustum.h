25583aa6ede0aabbb7e7239b4a50c4aeb418759e
shentongle <1275507634@qq.com>
2017-03-01 23:55:53 +0800

    frustum

--- src/Frustum.h
+++ src/Frustum.h
@@ -1,6 +1,8 @@
 #ifndef __FRUSTUM_H__
 #define __FRUSTUM_H__
 
+#include <Plane.h>
+
 namespace T3D {
 
 	enum ProjectionType
@@ -9,20 +11,47 @@ namespace T3D {
 		PT_PERSPECTIVE, //透视投影
 	};
 
+	enum FrustumPlaneType
+	{
+		FR_PLANE_NEAR = 0,
+		FR_PLANE_FAR,
+		FR_PLANE_RIGHT,
+		FR_PLANE_LEFT,
+		FR_PLANE_TOP,
+		FR_PLANE_BOTTOM,
+		FRUSTUM_PLANES
+	};
+
 	/************************************************************************/
 	/* 视锥体                                                                     */
 	/************************************************************************/
 	class Frustum
 	{
+	public:
+		Frustum();
+		Frustum(const Frustum &rhs);
+		Frustum(float near, float far, float xfov, float yfov, bool ortho = false);
+
+		virtual ~Frustum();
+
+		Frustum & operator=(const Frustum &rhs);
+
+		bool operator==(const Frustum &rhs);
+
+		void SetProjType(ProjectionType type);
+
+	protected:
+		void updateFrustum();
+
 	protected:
 		ProjectionType m_projType; //投影类型
 
-		float m_fov; //垂直方向的fov
+		float m_fov; //y方向的fov
 		float m_far; //远裁剪面的距离
 		float m_near; //近裁剪面的距离
 		float m_aspect; //宽高比
 
-		Plane m_planes[6];
+		Plane m_planes[FRUSTUM_PLANES];
 	};
 }
 #endif // !
